af5b73d6d1a3bf24cab553341d81e81e1021b465
Alexandru Moșoi <ami650@few.vu.nl>
2010-03-15 02:45:15 +0100

    Fixed running script

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-java -Dibis.cohort.impl="mt" -jar lib/structure.jar $*
+
+pushd `dirname $0`/.. &> /dev/null
+root=$PWD
+popd &> /dev/null
+
+echo $root
+
+prun -v -1 -np 1 java -ea                                \
+    -Dibis.pool.size="$procs"                            \
+    -Dibis.pool.name="structure-$RANDOM"                 \
+    -Dibis.cohort.impl="mt"                              \
+    -Dibis.cohort.workers="4"                            \
+    -cp ".:$root/log4j.properties:$root/lib/*"           \
+    ibis.structure.CohortLauncher $*
+
