4c66931418c3fe17ed3ad74c0f21f3eca535c3b8
Alexandru Moșoi <brtzsnr@gmail.com>
2010-09-02 01:39:00 -0400

    Implemented the reader.

--- src/main/java/ibis/structure/Reader.java
+++ src/main/java/ibis/structure/Reader.java
@@ -1,304 +1,77 @@
 package ibis.structure;
 
+import gnu.trove.TIntArrayList;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.util.Map;
-import java.util.Vector;
-
-import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.IConstr;
-import org.sat4j.specs.ISolver;
-import org.sat4j.specs.IVec;
-import org.sat4j.specs.IVecInt;
-import org.sat4j.specs.SearchListener;
-import org.sat4j.specs.TimeoutException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import org.apache.log4j.Logger;
 
 
 /**
  * Reader is used to read instances using the SAT4J reader.
  */
-public final class Reader implements ISolver {
-    private int numVariables;
-    private int numConstraints;
-    private VecInt[][] clauses;
+public final class Reader {
+  private static final Logger logger = Logger.getLogger(Reader.class);
 
-    public Reader() {
-        reset();
+  public static Skeleton parseText(String text)
+      throws IOException, ParseException {
+    return parseStream(new ByteArrayInputStream(text.getBytes("UTF-8")));
+  }
+
+  private static Skeleton parseStream(InputStream source)
+      throws IOException, ParseException {
+    Scanner scanner = new Scanner(source);
+
+    // Skip comments
+    try {
+      String token = scanner.next();
+      while (token.equals("c")) {
+        logger.debug("c" + scanner.nextLine());
+        token = scanner.next();
+      }
+      if (!token.equals("p")) {
+        throw new ParseException("Excepted 'p', but '" + token +
+                                 "' was found");
+      }
+    } catch (NoSuchElementException e) {
+      throw new ParseException("Header not found");
     }
 
-    public Skeleton skeleton() {
-        Skeleton skeleton = new Skeleton();
-        skeleton.numVariables = numVariables;
-
-        for (int i = 1; i <= 3; ++i)
-            for (int k = 0; k < clauses[i][0].size(); ++k)
-                for (int j = 0; j < i; ++j)
-                    skeleton.clauses[i].push(clauses[i][j].getAt(k));
-
-        return skeleton;
+    // Reads header
+    int numVariables, numClauses;
+    try {
+      String cnf = scanner.next();
+      if (!cnf.equals("cnf")) {
+        throw new ParseException("Expected 'cnf', but '" + cnf +
+                                 "' was found");
+      }
+      numVariables = scanner.nextInt();
+      numClauses = scanner.nextInt();
+    } catch (NoSuchElementException e) {
+      throw new ParseException("Incomplete header");
     }
 
-    /*** ISolver methods ***/
-
-    @Deprecated
-    public int newVar() {
-        throw new UnsupportedOperationException();
-    }
-
-    public int newVar(int howMany) {
-        if (numVariables != 0)
-            throw new UnsupportedOperationException(
-                    "Only one call to newVar(...) allowed");
-        numVariables = howMany;
-        return numVariables;
-    }
-
-    public int nextFreeVarId(boolean reserve) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setExpectedNumberOfClauses(int numClauses) {
-    }
-
-    private int extra(int var, boolean negated) {
-        return (numVariables - 1 + var) * 2 + (negated ? 1 : 0);
-    }
-
-    private void addUnit(int first)
-            throws ContradictionException {
-        ++numConstraints;
-        clauses[1][0].push(first);
-    }
-
-    private void addBinary(int first, int second)
-            throws ContradictionException {
-        ++numConstraints;
-        clauses[2][0].push(first);
-        clauses[2][1].push(second);
-    }
-
-    private void addTernary(int first, int second, int third)
-            throws ContradictionException {
-        ++numConstraints;
-        clauses[3][0].push(first);
-        clauses[3][1].push(second);
-        clauses[3][2].push(third);
-    }
-
-    public IConstr addClause(IVecInt literals)
-            throws ContradictionException {
-        if (literals.size() == 0)
-            return null;
-
-        // unit
-        int first = SAT.fromDimacs(literals.get(0));
-        if (literals.size() == 1) {
-            addUnit(first);
-            return null;
+    // Reads clauses
+    Skeleton skeleton = new Skeleton();
+    try {
+      TIntArrayList clause = new TIntArrayList();
+      while (numClauses > 0) {
+        int literal = scanner.nextInt();
+        if (literal == 0) {
+          skeleton.add(clause.toNativeArray());
+          clause.clear();
+          --numClauses;
+        } else {
+          clause.add(literal);
         }
-
-        // binary
-        int second = SAT.fromDimacs(literals.get(1));
-        if (literals.size() == 2) {
-            addBinary(first, second);
-            return null;
-        }
-
-        // ternary
-        int third = SAT.fromDimacs(literals.get(2));
-        if (literals.size() == 3) {
-            addTernary(first, second, third);
-            return null;
-        }
-
-        // longer, must transform
-        int k = literals.size();
-        for (int i = 2; i <= k - 3; ++i) {
-            int first_ = extra(i - 1, false);
-            int second_ = SAT.fromDimacs(literals.get(i));
-            int third_ = extra(i, true);
-            addTernary(first_, second_, third_);
-        }
-        addTernary(
-                SAT.fromDimacs(literals.get(0)),
-                SAT.fromDimacs(literals.get(1)),
-                extra(1, false));
-        addTernary(
-                extra(k - 3, true),
-                SAT.fromDimacs(literals.get(k - 2)),
-                SAT.fromDimacs(literals.get(k - 1)));
-        numVariables += k - 3;
-
-        return null;  /* intended because removeConstr(...) is not implemented */
+      }
+    } catch (NoSuchElementException e) {
+      throw new ParseException("Incomplete problem: " + numClauses +
+                               " clauses are missing");
     }
 
-    public IConstr addBlockingClause(IVecInt literals)
-            throws ContradictionException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean removeConstr(IConstr c) {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean removeSubsumedConstr(IConstr c) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void addAllClauses(IVec<IVecInt> clauses)
-            throws ContradictionException {
-        throw new UnsupportedOperationException();
-    }
-
-    public IConstr addAtMost(IVecInt literals, int degree)
-            throws ContradictionException {
-        throw new UnsupportedOperationException();
-    }
-
-    public IConstr addAtLeast(IVecInt literals, int degree)
-            throws ContradictionException {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setTimeout(int t) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setTimeoutOnConflicts(int count) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setTimeoutMs(long t) {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getTimeout() {
-        throw new UnsupportedOperationException();
-    }
-
-    public long getTimeoutMs() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void expireTimeout() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void reset() {
-        numVariables = 0;
-        numConstraints = 0;
-
-        clauses = new VecInt[4][];
-        for (int i = 0; i < 4; ++i) {
-            clauses[i] = new VecInt[i];
-            for (int j = 0; j < i; ++j)
-                clauses[i][j] = new VecInt();
-        }
-    }
-
-    @Deprecated
-    public void printStat(PrintStream out, String prefix) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void printStat(PrintWriter out, String prefix) {
-        throw new UnsupportedOperationException();
-    }
-
-    public Map<String, Number> getStat() {
-        throw new UnsupportedOperationException();
-    }
-
-    public String toString(String prefix) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void clearLearntClauses() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setDBSimplificationAllowed(boolean status) {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isDBSimplificationAllowed() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setSearchListener(SearchListener sl) {
-        throw new UnsupportedOperationException();
-    }
-
-    public SearchListener getSearchListener() {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isVerbose() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setVerbose(boolean value) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setLogPrefix(String prefix) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String getLogPrefix() {
-        throw new UnsupportedOperationException();
-    }
-
-
-    /*** IProblem methods ***/
-    public int[] model() {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean model(int var) {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isSatisfiable()
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isSatisfiable(IVecInt assumps, boolean globalTimeout)
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isSatisfiable(boolean globalTimeout)
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isSatisfiable(IVecInt assumps)
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public int[] findModel()
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public int[] findModel(IVecInt assumps)
-            throws TimeoutException {
-        throw new UnsupportedOperationException();
-    }
-
-    public int nConstraints() {
-        return numConstraints;
-    }
-
-    public int nVars() {
-        return numVariables;
-    }
-
-    public void printInfos(PrintWriter out, String prefix) {
-        throw new UnsupportedOperationException();
-    }
+    return skeleton;
+  }
 }
---
+++ src/test/java/ibis/structure/ReaderTest.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import org.junit.Test;
+
+import ibis.structure.ParseException;
+import ibis.structure.Reader;
+
+public class ReaderTest {
+  @Test(expected=ParseException.class)
+  public void missingHeader() throws Exception {
+    Reader.parseText("1 0\n" +
+                     "2 0\n" +
+                     "3 0\n");
+  }
+
+  @Test(expected=ParseException.class)
+  public void incompleteProblem() throws Exception {
+    Reader.parseText("p cnf 3 3\n" +
+                     "1 2 3 0\n");
+  }
+}
