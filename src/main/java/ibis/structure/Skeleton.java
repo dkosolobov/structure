7efbacadc6ae8ca1c1b4f0ee83fb0d8891f95f81
Alexandru Moșoi <brtzsnr@gmail.com>
2010-09-02 11:26:14 -0400

    Added function to cleanup instances.

--- src/main/java/ibis/structure/Skeleton.java
+++ src/main/java/ibis/structure/Skeleton.java
@@ -1,9 +1,10 @@
 package ibis.structure;
 
 import gnu.trove.TIntArrayList;
+import gnu.trove.TIntHashSet;
+import gnu.trove.TIntIterator;
 import java.io.Serializable;
 import org.apache.log4j.Logger;
-
 
 /**
  * @todo store initial number of variables
@@ -12,7 +13,6 @@ public final class Skeleton implements Serializable {
   private static final Logger logger = Logger.getLogger(Skeleton.class);
 
   public TIntArrayList[] clauses;
-
 
   /**
    * Parses a string to produce a skeleton instance
@@ -87,5 +87,83 @@ public final class Skeleton implements Serializable {
       clauses[0].add(clause);
       clauses[0].add(0);
     }
+  }
+
+  /**
+   * Canonicalize instance.
+   *
+   * Removes duplicate variables from clauses.
+   * Detects some simple contradictions like a &amp; -a.
+   * Simplifies clauses if literals are known, however clauses
+   * are passed only once.
+   */
+  public void canonicalize()
+      throws ContradictionException {
+    TIntHashSet units = new TIntHashSet(clauses[1].toNativeArray());
+
+    TIntArrayList binaries = new TIntArrayList();
+    for (int i = 0; i < clauses[2].size(); i += 2) {
+      int u = clauses[2].get(i);
+      int v = clauses[2].get(i + 1);
+      if (u == -v) {
+      } else if (u == v) {
+        if (units.contains(u)) {
+        } else if (units.contains(-u)) {
+          throw new ContradictionException();
+        } else {
+          units.add(u);
+        }
+      } else if (units.contains(u) || units.contains(v)) {
+      } else if (units.contains(-u)) {
+        if (units.contains(-v)) {
+          throw new ContradictionException();
+        } else {
+          units.add(v);
+        }
+      } else if (units.contains(-v)) {
+        units.add(u);
+      } else {
+        binaries.add(u);
+        binaries.add(v);
+      }
+    }
+
+    TIntArrayList clauses = new TIntArrayList();
+    TIntHashSet clause = new TIntHashSet();
+    boolean satisfied = false;
+    for (int i = 0; i < this.clauses[0].size(); ++i) {
+      int literal = this.clauses[0].get(i);
+      if (literal == 0) {
+        if (!satisfied) {
+          if (clause.size() == 0) {
+            throw new ContradictionException();
+          }
+          int[] clause_ = clause.toArray();
+          if (clause_.length == 1) {
+            units.add(clause_[0]);
+          } else if (clause_.length == 2) {
+            binaries.add(clause_);
+          } else {
+            clauses.add(clause_);
+            clauses.add(0);
+          }
+        }
+      } else if (units.contains(literal) || clause.contains(-literal)) {
+        satisfied = true;
+      } else if (!units.contains(-literal)) {
+        clause.add(literal);
+      }
+    }
+
+    TIntIterator it;
+    for (it = units.iterator(); it.hasNext(); ) {
+      if (units.contains(-it.next())) {
+        throw new ContradictionException();
+      }
+    }
+
+    this.clauses[0] = clauses;
+    this.clauses[1] = new TIntArrayList(units.toArray());
+    this.clauses[2] = binaries;
   }
 }
