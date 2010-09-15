5130cdb57ac9c49e87f07ef178b9436566da051f
Alexandru Moșoi <brtzsnr@gmail.com>
2010-09-15 18:06:26 +0200

    Added Cohort job.

---
+++ src/main/java/ibis/structure/Job.java
@@ -0,0 +1,80 @@
+package ibis.structure;
+
+import gnu.trove.TIntArrayList;
+import ibis.cohort.Activity;
+import ibis.cohort.ActivityIdentifier;
+import ibis.cohort.Event;
+import ibis.cohort.MessageEvent;
+import ibis.cohort.context.UnitActivityContext;
+
+public class Job extends Activity {
+  private final ActivityIdentifier parent;
+  private final Skeleton instance;
+  private final int branch;
+  private boolean firstReply = false;
+  private boolean solved = false;
+  private TIntArrayList units;
+
+  public Job(ActivityIdentifier parent, Skeleton instance, int branch) {
+    super(UnitActivityContext.DEFAULT);
+    this.parent = parent;
+    this.instance = instance;
+    this.branch = branch;
+  }
+    
+  @Override
+  public void initialize() {
+    try {
+      Solver solver = new Solver(instance, branch);
+      int literal = solver.lookahead();
+      units = solver.getAllUnits();
+
+      if (literal == 0) {
+        cohort.send(identifier(), parent, units.toNativeArray());
+        finish();
+      } else {
+        Skeleton skeleton = solver.skeleton(false);
+        cohort.submit(new Job(identifier(), skeleton, literal));
+        cohort.submit(new Job(identifier(), skeleton, -literal));
+        suspend();
+      }
+    } catch (ContradictionException e) {
+      cohort.send(identifier(), parent, null);
+      finish();
+    }
+  }
+
+  @Override
+  public void process(Event e) throws Exception {
+    int[] reply = ((MessageEvent<int[]>)e).message;
+    firstReply = !firstReply;
+
+    if (reply == null) {
+      if (firstReply) {
+        // Waits for the other branch to finish.
+        suspend();
+      } else {
+        cohort.send(identifier(), parent, null);
+        finish();
+      }
+    } else {
+      // TODO: The other branch should be canceled.
+      if (!solved) {
+        solved = true;
+        units.add(reply);
+        cohort.send(identifier(), parent, units.toNativeArray());
+      }
+      if (!firstReply) {
+        finish();
+      }
+    }
+  }
+
+  @Override
+  public void cleanup() throws Exception {
+  }
+
+  @Override
+  public void cancel() throws Exception {
+  }
+}
