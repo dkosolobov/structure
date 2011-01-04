01d7f5594389cfd5f567a083d8ae7c9ad57e6cc3
Alexandru Moșoi <brtzsnr@gmail.com>
2011-01-04 16:18:37 +0200

    Added download script.

---
+++ tests/sat09/archive/download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+# SAT09
+wget http://www.cril.univ-artois.fr/SAT09/bench/appli.7z
+wget http://www.cril.univ-artois.fr/SAT09/bench/crafted.7z
+wget http://www.cril.univ-artois.fr/SAT09/bench/random.7z
+wget http://www.cril.univ-artois.fr/SAT09/bench/random-additional.7z
+
+# SAT07
+wget http://www.satcompetition.org/2007/crafted.tar
+
