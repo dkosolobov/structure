a2f5d8e49b7a328a24069375dbd7d12f31e216a8
Alexandru Mosoi <ami650@few.cs.vu.nl>
2011-01-31 15:06:59 +0100

    Improved plot.

--- testing/plot/plot-solved-over-time.sh
+++ testing/plot/plot-solved-over-time.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 (
-  echo -e set terminal png
-  echo -e set output \"plot.png\"
+  echo -e set terminal png size 800 600
+  #echo -e set output \"plot.png\"
   echo -e set key top left
   echo -e set xlabel \"solved instances\"
   echo -e set ylabel \"seconds\"
+  echo -e set mxtics
+  echo -e set mytics
+  echo -e set grid
 
   header="plot "
   for file in $*; do
     data_file=`mktemp`
-    ./extract-solved-over-time.sh $file > $data_file
+    `dirname $0`/extract-solved-over-time.sh $file > $data_file
 
     echo -e -n "$header" \"$data_file\" title \"${file%.dat}\" with linespoints
     header=", \\\\\\n     "
