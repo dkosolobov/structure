ec7ffabf5f91cf4a60d4136a71b9a2072c190f1f
Alexandru Moșoi <brtzsnr@gmail.com>
2011-06-14 22:37:57 -0400

    Implemented selfsubsumming for all clause lengths.

--- src/main/java/ibis/structure/SelfSubsumming.java
+++ src/main/java/ibis/structure/SelfSubsumming.java
@@ -29,7 +29,7 @@ public final class SelfSubsumming {
   /** Used to check if a clause is included in another clause */
   private final TouchSet visited;
   /** Number of removed clauses */
-  private int numRemovedClauses = 0;
+  private int numRemovedLiterals = 0;
 
   /** Constructor */
   private SelfSubsumming(final Solver solver) {
@@ -51,22 +51,22 @@ public final class SelfSubsumming {
   /** @return true if any clause was removed. */
   private boolean run() throws ContradictionException {
     // prevIndex, last and clauses encode (very compact)
-    // 2 * numVariables single linked lists (one for each variable)
+    // numVariables single linked lists (one for each variable)
     // containing clauses where literal is the best literal.
     TIntArrayList prevIndex = new TIntArrayList();
-    int[] last = new int[2 * numVariables + 1];
+    int[] last = new int[numVariables + 1];
     java.util.Arrays.fill(last, -1);
 
-    // Computes clauses' hashes and bestLiterals.
+    // Computes clauses' hashes and bestVariables.
     // Best literal of a clause is the literal in the clause
     // that appears in minimum number of clauses in the formula.
     TIntArrayList clauses = getClauses();
     for (int i = 0; i < clauses.size(); i++) {
-      int clause = clauses.get(i);
+      int clause = clauses.getQuick(i);
+      int bestVariable = bestVariable(clause);
       hashes.put(clause, clauseHash(clause));
-      int bestLiteral = bestLiteral(clause);
-      prevIndex.add(last[bestLiteral + numVariables]);
-      last[bestLiteral + numVariables] = i;
+      prevIndex.add(last[bestVariable]);
+      last[bestVariable] = i;
     }
 
     // Clauses and their hashes containing current literal, u.
@@ -74,73 +74,54 @@ public final class SelfSubsumming {
     TIntArrayList uClauses = new TIntArrayList();
     TLongArrayList uHashes = new TLongArrayList();
     TouchSet touched = new TouchSet(numVariables);
-    for (int u = -numVariables; u <= numVariables; ++u) {
-      if (last[u + numVariables] == -1) {
+    for (int u = 1; u <= numVariables; ++u) {
+      if (last[u] == -1) {  // Empty list
         continue;
       }
 
       uClauses.reset();
       uHashes.reset();
-      TIntIterator it1 = watchLists.get(u).iterator();
+      TIntIterator it1;
+      
+      it1 = watchLists.get(u).iterator();
       for (int size = watchLists.get(u).size(); size > 0; size--) {
         int clause = it1.next();
         uClauses.add(clause);
         uHashes.add(hashes.get(clause));
       }
+      it1 = watchLists.get(neg(u)).iterator();
+      for (int size = watchLists.get(neg(u)).size(); size > 0; size--) {
+        int clause = it1.next();
+        uClauses.add(clause);
+        uHashes.add(hashes.get(clause));
+      }
 
-      touched.reset();
-      solver.graph.dfs(neg(u), touched);
-
-      int index = last[u + numVariables];
+      int index = last[u];
       while (index != -1) {
         int clause = clauses.get(index);
         if (!isClauseRemoved(formula, clause)) {
-          binarySelfSubsum(clause, u, touched);
           long hash = clauseHash(clause);
           hashes.put(clause, hash);
-          findSubsummed(clause, uClauses, uHashes);
+          findSelfSubsummed(clause, uClauses, uHashes);
         }
         index = prevIndex.get(index);
       }
     }
 
     if (Configure.verbose) {
-      if (numRemovedClauses > 0) {
-        System.err.print("ss" + numRemovedClauses + ".");
+      if (numRemovedLiterals > 0) {
+        System.err.print("ss" + numRemovedLiterals + ".");
       }
     }
-    return numRemovedClauses > 0;
-  }
-
-  /**
-   * If a + b + c + ... and -a &ge; -b
-   * then a + c + ...
-   */
-  private void binarySelfSubsum(final int clause,
-                                final int literal,
-                                final TouchSet touched)
-      throws ContradictionException {
-    if (type(formula, clause) != OR) {
-      return;
-    }
-
-    int length = length(formula, clause);
-    for (int j = clause; j < clause + length; j++) {
-      int u = formula.getQuick(j);
-      if (u != literal && touched.contains(neg(u))) {
-        watchLists.removeLiteralAt(clause, j);
-        length--;
-        j--;
-      }
-    }
+    return numRemovedLiterals > 0;
   }
 
   /**
    * Removes clauses in uClauses subsummed by clause.
    */
-  private void findSubsummed(final int clause,
-                             final TIntArrayList uClauses,
-                             final TLongArrayList uHashes)
+  private void findSelfSubsummed(final int clause,
+                                 final TIntArrayList uClauses,
+                                 final TLongArrayList uHashes)
       throws ContradictionException {
     long hash = hashes.get(clause);
     int length = length(formula, clause);
@@ -151,40 +132,71 @@ public final class SelfSubsumming {
       visited.add(formula.getQuick(j));
     }
 
+next_clause:
     for (int i = 0; i < uClauses.size(); i++) {
       int other = uClauses.getQuick(i);
       long otherHash = uHashes.getQuick(i);
 
+      // Fast inclusion check.
       if ((hash & ~otherHash) != 0
           || isClauseRemoved(formula, other)
           || other == clause) {
-        continue;
+        continue next_clause;
       }
+
+      // At this point there is a high probability that
+      // all variables of 'clause' are included in variables of 'other'.
 
       int otherLength = length(formula, other);
       int otherType = type(formula, other);
 
       int included = 0;
+      int selfLiteral = 0;   // at most one variable can have a different sign
+
       for (int j = other; j < other + otherLength; j++) {
-        if (visited.contains(formula.get(j))) {
+        int literal = formula.getQuick(j);
+        if (visited.contains(literal)) {
+          included++;
+        } else if (visited.contains(neg(literal))) {
+          if (selfLiteral != 0) {
+            // Two literals with different signs
+            continue next_clause;
+          }
+          assert literal != 0;
+          selfLiteral = literal;
           included++;
         }
       }
 
       if (included != length) {
         // Not all literals in clause are in other
-        continue;
+        continue next_clause;
       }
 
       /*
-      logger.info(clauseToString(formula, clause) + " subsumes "
-          + clauseToString(formula, other));
-          */
+      if (length >= 3 && selfLiteral != 0)
+        logger.info(clauseToString(formula, clause) + " subsumes "
+            + clauseToString(formula, other));
+            */
 
+      boolean recompute = false;
       if (type == OR && otherType == OR) {
-        numRemovedClauses++;
-        watchLists.removeClause(other);
+        if (selfLiteral == 0) {
+          // clause subsumes other
+          numRemovedLiterals += otherLength;
+          watchLists.removeClause(other);
+        } else {
+          // clause selfsubsumes other at selfLiteral
+          recompute = true;
+          numRemovedLiterals++;
+          watchLists.removeLiteral(other, selfLiteral);
+        }
       } else if (type != OR && otherType != OR) {
+        // clause subsumes other
+        assert selfLiteral == 0;
+        recompute = true;
+        numRemovedLiterals += length;
+
         if (type == XOR) {
           switchXOR(formula, other);
         }
@@ -194,7 +206,9 @@ public final class SelfSubsumming {
           int literal = formula.getQuick(j);
           watchLists.removeLiteral(other, literal);
         }
-
+      }
+      
+      if (recompute) {
         // recomputes other's hash
         otherHash = clauseHash(clause);
         hashes.put(other, otherHash);
@@ -205,9 +219,11 @@ public final class SelfSubsumming {
 
   /**
    * Returns clauses sorted by decreasing length.
+   *
+   * Performs a radix sort.
    */
   private TIntArrayList getClauses() {
-    Vector<TIntArrayList> clauses = new Vector<TIntArrayList>();
+    Vector<TIntArrayList> clauses = new Vector<TIntArrayList>(4);
 
     ClauseIterator it = new ClauseIterator(formula);
     int numClauses = 0;
@@ -244,7 +260,7 @@ public final class SelfSubsumming {
     long hash = 0;
     int length = length(formula, clause);
     for (int i = clause; i < clause + length; i++) {
-      int h = Hash.hash(formula.getQuick(i));
+      int h = Hash.hash(var(formula.getQuick(i)));
       hash |= 1L << ((h >>> 26) & 63);
     }
     return hash;
@@ -253,22 +269,25 @@ public final class SelfSubsumming {
   /**
    * Returns the literal in clause that appears the least
    * in other clauses.
+   *
+   * TODO: returns variable
    */
-  private int bestLiteral(final int clause) {
-    int bestLiteral = 0;
+  private int bestVariable(final int clause) {
+    int bestVariable = 0;
     int minNumClauses = Integer.MAX_VALUE;
     int length = length(formula, clause);
 
     for (int i = clause; i < clause + length; i++) {
       int literal = formula.get(i);
-      int numClauses = watchLists.get(literal).size();
+      int numClauses = watchLists.get(literal).size()
+                       + watchLists.get(neg(literal)).size();
 
       if (numClauses < minNumClauses) {
-        bestLiteral = literal;
+        bestVariable = literal;
         minNumClauses = numClauses;
       }
     }
 
-    return bestLiteral;
+    return var(bestVariable);
   }
 }
