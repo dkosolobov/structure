379ecc8f6a6faa4d584fde20a90b021f19e7eaa5
Alexandru Moșoi <brtzsnr@gmail.com>
2011-06-20 17:10:59 -0400

    Also does Hidden Literal Elimination.

--- src/main/java/ibis/structure/HiddenTautologyElimination.java
+++ src/main/java/ibis/structure/HiddenTautologyElimination.java
@@ -21,29 +21,26 @@ import static ibis.structure.Misc.*;
 public class HiddenTautologyElimination {
   private static final Logger logger = Logger.getLogger(HiddenTautologyElimination.class);
 
-  private final Solver solver;
-  private final int numVariables;
-  private final TIntArrayList formula;
-  private final TouchSet touched;
-  private final TIntArrayList tautologies;
+  private Solver solver;
+  private TouchSet touched;
+  private TIntArrayList tautologies;
+  private int numRemovedLiterals = 0;
 
   private HiddenTautologyElimination(final Solver solver) {
     this.solver = solver;
 
-    numVariables = solver.numVariables;
-    formula = solver.formula;
-    touched = new TouchSet(numVariables);
+    touched = new TouchSet(solver.numVariables);
     tautologies = new TIntArrayList();
   }
 
-  public static boolean run(final Solver solver) {
-    return (new HiddenTautologyElimination(solver)).run();
+  public static void run(final Solver solver) throws ContradictionException {
+    (new HiddenTautologyElimination(solver)).run();
   }
 
-  private boolean run() {
-    int numTautologies = 0;
-
-    for (int literal = -numVariables; literal <= numVariables; literal++) {
+  private void run() throws ContradictionException {
+    for (int literal = -solver.numVariables;
+        literal <= solver.numVariables;
+        literal++) {
       if (literal == 0
           || solver.numBinaries(literal) == 0
           || solver.watchLists.get(literal).isEmpty()) {
@@ -56,44 +53,49 @@ public class HiddenTautologyElimination {
       eliminate(literal);
 
       // Removes discovered tautologies.
-      numTautologies += tautologies.size();
       for (int i = 0; i < tautologies.size(); i++) {
         solver.watchLists.removeClause(tautologies.get(i));
       }
     }
 
     if (Configure.verbose) {
-      if (numTautologies > 0) {
-        System.err.print("hte" + numTautologies + ".");
+      if (numRemovedLiterals > 0) {
+        System.err.print("hte" + numRemovedLiterals + ".");
       }
     }
-    return numTautologies > 0;
   }
 
-  private void eliminate(final int literal) {
+  private void eliminate(final int literal) throws ContradictionException {
     TIntHashSet watchList = solver.watchLists.get(literal);
     TIntIterator it = watchList.iterator();
 
 clause_loop:
     for (int size = watchList.size(); size > 0; size--) {
       int clause = it.next();
-      int length = length(formula, clause);
+      int length = length(solver.formula, clause);
 
       if (length < 3) {
         continue;
       }
-      if (type(formula, clause) != OR) {
+      if (type(solver.formula, clause) != OR) {
         continue;
       }
 
       // Checks if clause is a tautology
       for (int j = clause; j < clause + length; j++) {
-        int v = formula.getQuick(j);
+        int v = solver.formula.getQuick(j);
         if (v != literal && touched.contains(v)) {
           // If literal + v + a + ... and -literal => v
           // then clause is tautology
+          numRemovedLiterals += length;
           tautologies.add(clause);
           continue clause_loop;
+        }
+
+        if (v != literal && touched.contains(neg(v))) {
+          numRemovedLiterals++;
+          length--;
+          solver.watchLists.removeLiteral(clause, v);
         }
       }
     }
