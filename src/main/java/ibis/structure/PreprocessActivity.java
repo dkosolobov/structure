d5d5811d996ebb818da28e9e602c499625ff4b14
Alexandru Moșoi <brtzsnr@gmail.com>
2011-08-02 13:15:20 +0200

    Does some initial preprocessing. Works on industrial instances where part of the inputs in the circuit are fixed.

--- src/main/java/ibis/structure/PreprocessActivity.java
+++ src/main/java/ibis/structure/PreprocessActivity.java
@@ -2,14 +2,15 @@ package ibis.structure;
 
 import gnu.trove.list.array.TDoubleArrayList;
 import ibis.constellation.ActivityIdentifier;
+import ibis.constellation.Event;
 import org.apache.log4j.Logger;
 
 /**
  * Starts preprocesses the input instance.
  */
 public final class PreprocessActivity extends Activity {
-  private static final Logger logger = Logger.getLogger(
-      PreprocessActivity.class);
+  /** Core after simplification. */
+  private Core core = null;
 
   public PreprocessActivity(final ActivityIdentifier parent,
                             final ActivityIdentifier tracer,
@@ -19,7 +20,47 @@ public final class PreprocessActivity extends Activity {
 
   @Override
   public void initialize() throws Exception {
-    executor.submit(new XORActivity(parent, tracer, null, instance));
+    Solver solver = null;
+    Solution solution = null;
+    Normalizer normalizer = new Normalizer();
+
+    try {
+      normalizer.normalize(instance);
+      solver = new Solver(instance);
+
+      solver.propagate();
+      HyperBinaryResolution.run(solver);
+      HiddenTautologyElimination.run(solver);
+      PureLiterals.run(solver);
+      MissingLiterals.run(solver);
+
+      solution = solver.solve();
+      solution = normalizer.denormalize(solution);
+      assert !solution.isUnsatisfiable();
+    } catch (ContradictionException e) {
+      solution = Solution.unsatisfiable();
+    }
+
+    if (!solution.isUnknown()) {
+      reply(solution);
+      finish();
+      return;
+    }
+
+    core = solver.core();
+    normalizer.denormalize(core);
+    executor.submit(new XORActivity(identifier(), tracer, core.instance()));
+    suspend();
+  }
+
+  @Override
+  public void process(final Event e) throws Exception {
+    Solution solution = (Solution) e.data;
+    if (solution.isSatisfiable()) {
+      solution = core.merge(solution);
+    }
+
+    reply(solution);
     finish();
   }
 }
